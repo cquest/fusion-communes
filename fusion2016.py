573e2058aa6418c6ee53ffd3aafa512993660353
Christian Quest <cquest@openstreetmap.fr>
2015-12-30 12:33:55 +0100

    admin_level:proposed + tag source, population

--- fusion2016.py
+++ fusion2016.py
@@ -16,13 +16,14 @@ with open('fusion.csv') as fichierfusions:
    if fusion['dep']==sys.argv[1]:
     # construction de la requête overpass pour récupérer les infos sur les communes à fusionner
     communes = fusion['anciennes'].replace(',','|')
-    overpass = """http://overpass-api.de/api/interpreter?data=[out:json];relation["ref:INSEE"~"^%s"][name~"^(%s)$",i][admin_level=8];out;""" % (fusion['dep'], communes)
+    overpass = """http://overpass-api.de/api/interpreter?data=[out:json];relation["ref:INSEE"~"^%s"][name~"^(%s)$",i][admin_level~"(8|9|10)"];out;""" % (fusion['dep'], communes)
     osm = requests.get(overpass)
     outer = [] # pour accumuler les way en outer de la nouvelle commune
     inner = [] # pour accumuler les ways en inner de la nouvelle commune
     population = 0
     objlist = ""
     admin_centre = ""
+    insee = ""
     osm_json = json.loads(osm.text) # transforme réponse HTTP (text) en dictionnaire (json)
     for element in osm_json['elements']: # les différentes communes
       objlist = "r"+str(element['id'])+','+objlist
@@ -40,36 +41,64 @@ with open('fusion.csv') as fichierfusions:
         if member['type']=='node' and element['tags']['name']==fusion['chflieu']:
           admin_centre = """<member type='node' ref='%s' role='admin_centre' />""" % member['ref']
 #      requests.get("""http://localhost:8111/import?url=http://api.openstreetmap.org/api/0.6/relation/%s/full""" % element['id'])
+
+
     # prend la population du fichier CSV où à défaut celle calculée
-    population_tag = """<tag k='population' v='%s' />""" % fusion.get('population',str(population))
+    if fusion['population'] != "":
+      population_tag = """<tag k='population' v='%s' />""" % fusion['population']
+    else:
+      if population > 0 :
+        population_tag = """<tag k='population' v='%s' />""" % str(population)
+      else:
+        population_tag = ""
 
-    overpass = """http://overpass-api.de/api/interpreter?data=[out:json];relation["ref:INSEE"~"^%s"][name~"^(%s)$",i];out;""" % (fusion['dep'], fusion['nouvelle'])
-    osm = requests.get(overpass)
-    nouvelle_json = json.loads(osm.text) # transforme réponse HTTP (text) en dictionnaire (json)
-
+    # liste des objets à passer en nouvel admin_level
     for ref in inner:
       objlist = "w" + str(ref) + "," + objlist
-    requests.get("""http://localhost:8111/load_object?new_layer=true&objects=%s&addtags=admin_level=10&relation_members=true""" % objlist)
+    requests.get("""http://localhost:8111/load_object?new_layer=true&objects=%s&addtags=admin_level:proposed=10&relation_members=true""" % objlist)
     
-    outer_ways=""
-    for way in outer:
-      outer_ways=outer_ways+"""<member type='way' ref='%s' role='outer' />""" % way
 
-    newrel = """<?xml version='1.0' encoding='UTF-8'?>
+    overpass = """http://overpass-api.de/api/interpreter?data=[out:json];relation["start_date"~"^2016"]["ref:INSEE"~"^%s"][name~"^(%s)$",i];out;""" % (fusion['dep'], fusion['nouvelle'])
+    osm = requests.get(overpass)
+    nouvelle_json = json.loads(osm.text) # transforme réponse HTTP (text) en dictionnaire (json)
+    if len(nouvelle_json['elements']) == 0:
+      # la nouvelle commune n'existe pas, on la crée
+      outer_ways=""
+      for way in outer: # liste des way en 'outer'
+        outer_ways=outer_ways+"""<member type='way' ref='%s' role='outer' />""" % way
+
+      # tag ref:INSEE hérité du chef-lieu... ou pas
+      if insee == "":
+        print("%s : Pas de code INSEE ni admin_centre" % fusion['nouvelle'])
+        insee_tag = """<tag k='fixme' v='ref:INSEE inconnu et admin_centre manquant' />"""
+      else:
+        insee_tag = """<tag k='ref:INSEE' v='%s' /><tag k='fixme' v='ref:INSEE fixé arbitrairement à celui du chef-lieu, à vérifier avec COG2016' />""" % insee
+
+      # tag source si colonne jorf renseignée
+      if fusion['jorf'] != "":
+        source_tag = """<tag k='source' v='Journal Officiel: %s' />""" % fusion['jorf']
+      else:
+        source_tag = ""
+
+      newrel = """<?xml version='1.0' encoding='UTF-8'?>
 <osm version='0.6' upload='true' generator='fusion2016.py'>
-  <relation id='-%s' action='modify' visible='true'>
+  <relation id='-1' action='modify' visible='true'>
     <tag k='type' v='boundary' />
     <tag k='boundary' v='administrative' />
-    <tag k='admin_level' v='8' />
+    <tag k='admin_level:proposed' v='8' />
     <tag k='start_date' v='2016-01-01' />
-    <tag k='fixme' v='ref:INSEE fixé arbitrairement à celui du chef-lieu, à vérifier avec COG2016' />
-    <tag k='ref:INSEE' v='%s' />
+    %s
     <tag k='name' v='%s' />
     %s
     %s
     %s
+    %s
   </relation>
-</osm>""" % (insee, insee, fusion['nouvelle'],population_tag,admin_centre,outer_ways)
+</osm>""" % (insee_tag, fusion['nouvelle'],population_tag,source_tag,admin_centre,outer_ways)
 
-    requests.get("""http://localhost:8111/load_data?data="""+urllib.parse.quote_plus(newrel))
+      requests.get("""http://localhost:8111/load_data?data="""+urllib.parse.quote_plus(newrel))
+    else:
+      # la nouvelle commune existe déjà, on la charge en positionnant les tags voulus
+      nouvelle_id = "r" + str(nouvelle_json['elements'][0]['id'])
+      requests.get("""http://localhost:8111/load_object?&objects=%s&addtags=admin_level:proposed=8|start_date=2016-01-01|ref:INSEE=%s|name=%s&relation_members=true""" % (nouvelle_id, insee,fusion['nouvelle']))
 
